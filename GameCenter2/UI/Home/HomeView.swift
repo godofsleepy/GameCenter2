719d09361b9f90146279ed0f8216f2185994e42a
Rifat Khadafy <45101689+godofsleepy@users.noreply.github.com>
2021-08-30 12:03:15 +0700

    add padding

--- GameCenter2/UI/Home/HomeView.swift
+++ GameCenter2/UI/Home/HomeView.swift
@@ -16,88 +16,87 @@ struct HomeView: View {
     @State var selection: Int? = nil
     
     var body: some View {
-        
         NavigationView{
             ZStack {
                 Color("purple").edgesIgnoringSafeArea(.all)
-                
                 ScrollView {
-                    ZStack{
-                        RoundedRectangle(cornerRadius: 20)
-                            .stroke(Color("pink"), lineWidth: 5)
-                        
-                        HStack {
-                            Button(action: {
-                                print("Left")
-                                self.index -= 1
-                                if self.index < 0{
-                                    self.index = 3
-                                }
-                                self.presenter.getGames(platformId: dataPlatforms[self.index].id)
-                            }) {
-                                Image("chevron.left")
-                                    .foregroundColor(Color(red: 247 / 255, green: 164 / 255, blue: 10 / 255))
-                            }
+                    VStack {
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 20)
+                                .stroke(Color("pink"), lineWidth: 5)
                             
-                            CarouselView(index: self.$index.animation(), maxIndex: 3) {
-                                ForEach(dataPlatforms, id: \.self) { platform in
-                                    Image(platform.image)
-                                        .resizable()
-                                        .scaledToFit()
+                            HStack {
+                                Button(action: {
+                                    print("Left")
+                                    self.index -= 1
+                                    if self.index < 0{
+                                        self.index = 3
+                                    }
+                                    self.presenter.getGames(platformId: dataPlatforms[self.index].id)
+                                }) {
+                                    Image("chevron.left")
+                                        .foregroundColor(Color(red: 247 / 255, green: 164 / 255, blue: 10 / 255))
                                 }
-                            }
-                            
-                            Button(action: {
-                                print("Right")
-                                self.index += 1
-                                if self.index == 4{
-                                    self.index = 0
-                                }
-                                self.presenter.getGames(platformId: dataPlatforms[self.index].id)
                                 
-                            }) {
-                                Image("chevron.right")
-                                    .foregroundColor(Color("orange"))
+                                CarouselView(index: self.$index.animation(), maxIndex: 3) {
+                                    ForEach(dataPlatforms, id: \.self) { platform in
+                                        Image(platform.image)
+                                            .resizable()
+                                            .scaledToFit()
+                                    }
+                                }
+                                
+                                Button(action: {
+                                    print("Right")
+                                    self.index += 1
+                                    if self.index == 4{
+                                        self.index = 0
+                                    }
+                                    self.presenter.getGames(platformId: dataPlatforms[self.index].id)
+                                    
+                                }) {
+                                    Image("chevron.right")
+                                        .foregroundColor(Color("orange"))
+                                }
                             }
+                            .padding(10)
+                            
+                            VStack(alignment: .leading) {
+                                Text("platform")
+                                    .foregroundColor(Color.white).font(.system(size: 16))
+                                Text(dataPlatforms[self.index].name)
+                                    .foregroundColor(Color(red: 241 / 255, green: 79 / 255, blue: 114 / 255)).font(.system(size: 33,weight: .semibold))
+                            }
+                            .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: 80, alignment: .topLeading)
+                            .padding(30)
+                            
                         }
-                        .padding(10)
+                        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 250, maxHeight: 250)
+                        .padding(.bottom)
                         
-                        VStack(alignment: .leading) {
-                            Text("platform")
-                                .foregroundColor(Color.white).font(.system(size: 16))
-                            Text(dataPlatforms[self.index].name)
-                                .foregroundColor(Color(red: 241 / 255, green: 79 / 255, blue: 114 / 255)).font(.system(size: 33,weight: .semibold))
-                        }
-                        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: 80, alignment: .topLeading)
-                        .padding(30)
+                        Text("Featured")
+                            .foregroundColor(Color("pink"))
+                            .font(.system(size: 24))
+                            .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
                         
-                    }
-                    .frame(minWidth: 0, maxWidth: .infinity, minHeight: 250, maxHeight: 250)
-                    .padding(.bottom)
-                    
-                    Text("Featured")
-                        .foregroundColor(Color("pink"))
-                        .font(.system(size: 24))
-                        .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
-                    
-                    VStack{
-                        if presenter.loadingState {
-                            Spinner(isAnimating: true, style: .large).eraseToAnyView()
-                        } else {
-                            ForEach(self.presenter.games, id: \.id){ game in
-                                self.presenter.linkBuilder(for: String(game.id)) {
-                                    GameItemView(game: game)
+                        VStack{
+                            if presenter.loadingState {
+                                Spinner(isAnimating: true, style: .large).eraseToAnyView()
+                            } else {
+                                ForEach(self.presenter.games, id: \.id){ game in
+                                    self.presenter.linkBuilder(for: String(game.id)) {
+                                        GameItemView(game: game)
+                                    }
                                 }
                             }
                         }
-                    }
+                    }.padding()
                     
                 }
-                .padding(24)
                 
                 
-            } // ZStack
-            .navigationBarTitle("Home")
+            }
+            .navigationBarTitle("Home", displayMode: .automatic)
             .navigationBarItems(trailing: NavigationLink(destination: ProfileView(),tag: 1,selection: $selection){
                 Button(action: {
                     self.selection = 1
--- GameCenter2/UI/Search/SearchView.swift
+++ GameCenter2/UI/Search/SearchView.swift
@@ -12,21 +12,21 @@ struct SearchView: View {
     
     @EnvironmentObject var presenter : SearchPresenter
     @State var query: String = ""
-
+    
     var body: some View {
-
+        
         NavigationView{
             ZStack{
                 Color("purple").edgesIgnoringSafeArea(.all)
-                VStack {
+                ScrollView{
                     HStack {
                         HStack {
                             Image(systemName: "magnifyingglass").foregroundColor(Color("orange"))
                             TextField("Search", text:$query, onCommit: {
                                 self.presenter.getSearch(query: query)
                             }).foregroundColor(Color.white)
-
-
+                            
+                            
                             if !query.isEmpty {
                                 Button(action: {
                                     self.query = ""
@@ -37,31 +37,30 @@ struct SearchView: View {
                         }
                         .padding()
                         .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color("orange"), lineWidth: 1))
-
+                        
                     }.padding(.horizontal).padding(.vertical)
-
-                    ScrollView{
-                        VStack{
-                            if presenter.loadingState {
-                                Spinner(isAnimating: true, style: .large).eraseToAnyView()
-                            } else {
-                                if !self.presenter.games.isEmpty {
-                                    ForEach(self.presenter.games, id: \.id){ game in
-                                        self.presenter.linkBuilder(for: String(game.id)) {
-                                            GameItemView(game: game)
-                                        }
+                    
+                    VStack{
+                        if presenter.loadingState {
+                            Spinner(isAnimating: true, style: .large).eraseToAnyView()
+                        } else {
+                            if !self.presenter.games.isEmpty {
+                                ForEach(self.presenter.games, id: \.id){ game in
+                                    self.presenter.linkBuilder(for: String(game.id)) {
+                                        GameItemView(game: game)
                                     }
-
-                                } else {
-                                    Image("noFind").frame(alignment: .center).padding(.top, 50)
                                 }
                                 
+                            } else {
+                                Image("noFind").frame(alignment: .center).padding(.top, 50)
                             }
-
                             
-                        }.padding(.horizontal)
-                    }
+                        }
+                        
+                        
+                    }.padding(.horizontal)
                 }
+                
             }
             .navigationBarTitle("Search")
         }
